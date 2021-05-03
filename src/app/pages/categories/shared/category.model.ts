2b631711783b52a6076a185cb52f111cdffd6fac
Denis Pessoa <dnspessoa1@gmail.com>
2021-05-03 18:39:02 -0300

    add category service

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,8 +1,7 @@
 export class Category {
 
     constructor(public id?: number,
-                public name?: string,
-                public description?: string) {
-
+        public name?: string,
+        public description?: string) {
     }
 }
